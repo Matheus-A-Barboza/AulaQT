ccda763df9bcd8a47a5918fe2af832b7cf4c5d3a
Matheus <progammerantunes@gmail.com>
2023-09-12 21:52:05 -0300

    Atividade de Cadastro de Estoque

--- controleEstoque/controleDeEstoque.py
+++ controleEstoque/controleDeEstoque.py
@@ -1,19 +1,27 @@
+import sqlite3
 import sys
+from datetime import datetime
+
 from PySide6.QtWidgets import QMainWindow, QApplication, QHBoxLayout, QVBoxLayout, QTableWidget, QAbstractItemView, \
-    QPushButton, QLineEdit, QLabel, QWidget
+    QPushButton, QLineEdit, QLabel, QWidget, QMessageBox
 
 
-class Estoque (QMainWindow):
+class Estoque(QMainWindow):
     def __init__(self):
         super().__init__()
 
         self.setWindowTitle('Controle de Estoque')
         self.setGeometry(100, 100, 800, 400)
 
+        self.conn = sqlite3.connect('estoque.db')
+        self.cursor = self.conn.cursor()
+
+        self.criar_tabela()
+
         layout_principal = QHBoxLayout()
-        layout_esquerda = QHBoxLayout()
-        layout_direita = QHBoxLayout()
-        layout_botoes = QVBoxLayout()
+        layout_esquerda = QVBoxLayout()
+        layout_direita = QVBoxLayout()
+        layout_botoes = QHBoxLayout()
 
         self.tbl_produtos = QTableWidget()
         self.tbl_produtos.verticalHeader().setVisible(False)
@@ -25,6 +33,7 @@ class Estoque (QMainWindow):
         self.btn_cadastar = QPushButton('Cadastrar')
         self.btn_remover = QPushButton('Remover')
         self.btn_editar = QPushButton('Editar')
+        self.btn_limpar = QPushButton('Limpar Campos')
 
         layout_botoes.addWidget(self.btn_editar)
         layout_botoes.addWidget(self.btn_remover)
@@ -55,6 +64,8 @@ class Estoque (QMainWindow):
         layout_esquerda.addWidget(self.lbl_fornecedor)
         layout_esquerda.addWidget(self.txt_fornecedor)
         layout_esquerda.addWidget(self.btn_cadastar)
+        # layout_esquerda.addWidget(self.btn_limpar_banco)
+        layout_esquerda.addWidget(self.btn_limpar)
         layout_esquerda.addLayout(layout_botoes)
 
         layout_principal.addLayout(layout_esquerda)
@@ -63,6 +74,68 @@ class Estoque (QMainWindow):
         central_widget = QWidget()
         central_widget.setLayout(layout_principal)
         self.setCentralWidget(central_widget)
+
+        self.btn_cadastar.clicked.connect(self.inserir_produtos)
+        self.btn_limpar.clicked.connect(self.limpar_campos)
+
+    def criar_tabela(self):
+        try:
+            self.cursor.execute('''
+                CREATE TABLE IF NOT EXISTS produtos
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                nome TEXT NOT NULL,
+                preco REAL TEXT NOT NULL,
+                quantidade INT NOT NULL,
+                data_validade TEXT,
+                categoria TEXT NOT NULL,
+                fornecedor TEXT NOT NULL
+                )''')
+            self.conn.commit()
+        except Exception as e:
+            print(e)
+
+    def inserir_produtos(self):
+        if self.validar_data():
+            try:
+                self.cursor.execute(
+                    "INSERT INTO produtos (nome, preco,"
+                    "quantidade, data_validade, categoria,"
+                    "fornecedor) VALUES (?, ?, ?, ?, ?, ?)", (self.txt_nome.text(),
+                                                              self.txt_preco.text(),
+                                                              self.txt_qnt.text(),
+                                                              self.txt_data.text(),
+                                                              self.txt_categoria.text(),
+                                                              self.txt_fornecedor.text()))
+                self.conn.commit()
+                QMessageBox.information(self, 'Cadastro de Produto', 'Produto Adicionado com Sucesso')
+                self.limpar_campos()
+
+            except Exception as e:
+                print(e)
+
+    # def limpar_banco(self):
+    #     try:
+    #         self.cursor.execute(
+    #             "TRUNCATE TABLE produtos"
+    #         )
+    #         self.conn.commit()
+    #     except Exception as e:
+    #         print(e)
+
+    def limpar_campos(self):
+        self.txt_nome.clear()
+        self.txt_preco.clear()
+        self.txt_qnt.clear()
+        self.txt_data.clear()
+        self.txt_categoria.clear()
+        self.txt_fornecedor.clear()
+
+    def validar_data(self):
+        try:
+            datetime.strptime(self.txt_data.text(), '%d,%m,%Y')
+            return True
+        except:
+            QMessageBox.warning(self, 'Aviso', 'Data de Validade fora do padrão dd/mm/aaaa')
 
 
 if __name__ == '__main__':
